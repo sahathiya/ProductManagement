6091d4d9c8e191a2f6214a025fe9db90b8c51b5b
sahathiya <salamkaraya4@gmail.com>
2025-05-19 00:17:45 +0530

    set up deployment

--- Client/src/utils/axiosInstance.js
+++ Client/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4000', 
+  baseURL: 'https://productmanagement-98n0.onrender.com', 
   withCredentials: true,
 
   
