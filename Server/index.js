d8b881388456039621b429ebbacbe7e06854525b
sahathiya <salamkaraya4@gmail.com>
2025-05-19 00:18:16 +0530

    set up deployment

--- Server/index.js
+++ Server/index.js
@@ -11,7 +11,7 @@ const PORT=process.env.PORT||5000
 const app=express()
 app.use(express.json())
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://product-management-sahathiyas-projects.vercel.app',
   credentials: true
 }));
 
