860d4f9ee00227afdc8713530bbd7acfa25c5a60
sahathiya <salamkaraya4@gmail.com>
2025-05-20 10:45:20 +0530

    set up the style of the component

--- Client/src/pages/Landing.jsx
+++ Client/src/pages/Landing.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 function Landing() {
     const navigate=useNavigate()
+    const activeUser=useSelector((state)=>state.user.activeUser)
   return (
     <div className="w-full  h-screen  bg-primary text-white flex flex-col justify-center items-center p-10 relative font-poppins">
       <h2 className="text-3xl font-bold mb-4">Hello Friend!</h2>
@@ -10,11 +12,20 @@ function Landing() {
         Enter your personal details and <br />
         start your journey with us
       </p>
-      <button
+      {activeUser===null?(
+  <button
       onClick={()=>navigate('/signup')} 
       className="border border-white px-8 py-2 rounded-full hover:bg-white hover:text-[#004071] transition">
         GET STARTED
       </button>
+      ):(
+          <button
+      onClick={()=>navigate('/dashboard')} 
+      className="border border-white px-8 py-2 rounded-full hover:bg-white hover:text-[#004071] transition">
+        GO TO DASHBOARD
+      </button>
+      )}
+    
 
       <div className="absolute top-8 left-8 w-6 h-6 rotate-45 bg-[#2C6C8C]"></div>
       <div className="absolute bottom-8 left-8 w-40 h-40 rounded-full bg-[#2C6C8C] opacity-50"></div>
